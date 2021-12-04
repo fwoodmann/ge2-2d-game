5ef631098880489c47098a39b3d9c0ec4db92e98
da-albrecht <albrecht.david3@gmail.com>
2021-12-04 15:05:53 +0100

    added Options Menu and Volume Slider

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -9,4 +9,10 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
 }
