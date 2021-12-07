6c15c6bcc5faaefd536aac380ed96d70e2a06d7b
da-albrecht <albrecht.david3@gmail.com>
2021-12-07 16:17:24 +0100

    added camera movement, and deathzone moves with camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,17 +10,20 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float velocity = 0.0f;
     [SerializeField] private float jumpHeight = 0.0f;
     [SerializeField] private BoxCollider2D boxCollider2D;
-    
+    Camera m_Camera;
+    Vector3 cameraPos;
     void Start()
     {
+         m_Camera = Camera.main;
+         m_Camera.enabled = true;
     }
-
     void Update()
     {
         if(IsGrounded())
         {
             this.rb.velocity = new Vector2(0, jumpHeight);
-           // Debug.Log("up");
+            //Debug.Log(rb.position.y);
+            m_Camera.transform.position = new Vector3(0, rb.position.y + 4, -10);
         }
         if(Input.GetKey (KeyCode.D))
         {
@@ -30,7 +33,9 @@ public class PlayerMovement : MonoBehaviour
         {
             this.rb.velocity = new Vector2(-velocity, rb.velocity.y);
         }
+   
     }
+      
     
     /*
      * https://www.youtube.com/watch?v=ptvK4Fp5vRY
